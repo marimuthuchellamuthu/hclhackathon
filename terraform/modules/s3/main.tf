4d4fb810063f179b2ee6e6a29f887554831e12ac
marimuthuchellamuthu <marimuthu.c77@gmail.com>
2025-02-15 09:50:48 +0000

    commit

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "hcl-hackathon-healthcare"
-    key            = "state/terraform.tfstate"
+    key            = "state.tfstate"
     region         = "us-east-1"
     dynamodb_table = "hcl-hackathon-healthcare"
     encrypt        = true
