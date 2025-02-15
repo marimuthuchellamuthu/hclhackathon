dd7ce44f0fb0508982678c1d6df1ed6313651f0d
marimuthuchellamuthu <marimuthu.c77@gmail.com>
2025-02-15 13:34:20 +0000

    terraform final code update

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,14 +4,14 @@ resource "aws_vpc" "healthcare-vpc" {
 }
 
 resource "aws_subnet" "subnet_public_1" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.healthcare-vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet_private_1" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.healthcare-vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1a"
 }
@@ -37,76 +37,45 @@ resource "aws_security_group" "allow_all" {
 }
 
 # ECS
-resource "aws_ecs_cluster" "healthcare-ecs" {
-  name = "hcl-hackathon-healthcare"
-}
 
-resource "aws_ecs_task_definition" "patient_service" {
-  family                = "patient-service"
-  execution_role_arn    = "arn:aws:iam::539935451710:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
-  task_role_arn         = "arn:aws:iam::539935451710:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
-  network_mode          = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                   = "256"
-  memory                = "512"
+resource "aws_ecs_task_definition" "healthcare" {
+  family = "healthcare"
+  container_definitions = jsonencode([
+    {
+      name      = "patient"
+      image     = "539935451710.dkr.ecr.us-east-1.amazonaws.com/hcl-hackathon/healthcare:patient-app"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    },
+    {
+      name      = "appointment"
+      image     = "539935451710.dkr.ecr.us-east-1.amazonaws.com/hcl-hackathon/healthcare:appointment-app"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3001
+          hostPort      = 3001
+        }
+      ]
+    }
+  ])
 
-  container_definitions = jsonencode([{
-    name      = "patient-service"
-    image     = "539935451710.dkr.ecr.us-east-1.amazonaws.com/hcl-hackathon/healthcare:patient-app"
-    portMappings = [
-      {
-        containerPort = 3000
-        hostPort      = 3000
-        protocol      = "tcp"
-      }
-    ]
-    }])
-}
-
-resource "aws_ecs_task_definition" "appointment_service" {
-  family                = "appointment-service"
-  execution_role_arn    = "arn:aws:iam::539935451710:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
-  task_role_arn         = "arn:aws:iam::539935451710:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
-  network_mode          = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                   = "256"
-  memory                = "512"
-
-  container_definitions = jsonencode([{
-    name      = "appointment-service"
-    image     = "539935451710.dkr.ecr.us-east-1.amazonaws.com/hcl-hackathon/healthcare:appointment-app"
-    portMappings = [
-      {
-        containerPort = 3001
-        hostPort      = 3001
-        protocol      = "tcp"
-      }
-    ]
-    }])
-}
-
-resource "aws_ecs_service" "patient_service" {
-  name            = "patient-service"
-  cluster         = healthcare-ecs.patient_service.id
-  task_definition = aws_ecs_task_definition.patient_service.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = [aws_subnet.subnet_public_1.id]
-    security_groups = [aws_security_group.allow_all.id]
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
   }
-}
 
-resource "aws_ecs_service" "appointment_service" {
-  name            = "appointment-service"
-  cluster         = healthcare-ecs.appointment_serivce.id
-  task_definition = aws_ecs_task_definition.appointment_service.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = [aws_subnet.subnet_public_1.id]
-    security_groups = [aws_security_group.allow_all.id]
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
   }
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
-  access_key = "hcl-hackathon-access-key"
-  secret_key = "AKIAX3NVJFI7CSHQPSFG"
+  region  = "us-east-1"
+  #  access_key = "hcl-hackathon-access-key"
+  #  secret_key = "AKIAX3NVJFI7CSHQPSFG"
 }
