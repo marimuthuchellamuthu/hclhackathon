f18ffa1434d4efdec777e35349e607652ea50938
marimuthuchellamuthu <marimuthu.c77@gmail.com>
2025-02-15 09:50:36 +0000

    final

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -1,5 +1,5 @@
-resource "aws_ecs_cluster" "example_cluster" {
-  name = "example-cluster"
+resource "aws_ecs_cluster" "hcl-hackathon-healthcare" {
+  name = "hcl-hackathon-healthcare"
 }
 
 
--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "hcl-hackathon-healthcare"
-    key            = "state.tfstate"
+    key            = "state/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "hcl-hackathon-healthcare"
     encrypt        = true
